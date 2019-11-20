5fd584ecdb22885753e416e5338ce7d606de731d
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-20 16:22:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
 
   def self.new_by_filename(filename)
     song = Song.new(filename)
-   song.title = filename.split(" - ")[1]
+   song.name = filename.split(" - ")[1]
    puts song
   end
 
