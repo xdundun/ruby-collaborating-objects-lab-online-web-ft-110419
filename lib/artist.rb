e51d9a831b64cd6362fbfc5e91c39cd0ad6e6c5b
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-20 15:59:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,7 +17,7 @@ class Artist
 
   def self.find_or_create_by_name(name)
     if
-      Artist.all.select {|artist| artist.name == name }
+    #  Artist.all.select {|artist| artist.name == name }
     else
       artist = Artist.new(name)
        return artist
