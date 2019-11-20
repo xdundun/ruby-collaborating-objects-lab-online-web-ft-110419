e9082fbd7eba7b9cd493b43ce922f61700351673
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-20 16:10:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,7 +20,7 @@ class Artist
    end
 
    def self.find_or_create_by_name(name)
-     self.find_by_name(artist_name) || self.new(name)
+     self.find_by_name(name) || self.new(name)
    end
   # def self.find_or_create_by_name(name)
   #   if
