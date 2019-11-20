107dc391ca41652ff8e737cf0304d9fdabba45e6
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-20 16:24:40 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,24 @@
+class MP3Importer
+  attr_accessor :filenames, :path
+
+  def initialize(path)
+    @path = path
+    @filenames = []
+  end
+
+  def files
+    Dir.entries(path).each do |filename|
+      @filenames << "#{filename}"
+    end
+    @filenames.delete_if {|x| x == "." || x == ".."}
+  end
+
+  def import
+    @filenames.each do |filename|
+      filename.split(" - ")[2] = artist_name
+      Artist.find_or_create_by_name(artist_name)
+      filename.split(" - ")[1] = song
+      Artist.add_song(song)
+    end
+  end
+end
