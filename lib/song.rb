405a43c91b968ea1d56856f40ccca9c1ed69acfe
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-20 16:31:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,8 @@ class Song
   end
 
   def artist_name=(name)
-    Artist.find_or_create_by_name(name)
-    artist.name if artist
+  artist =   Artist.find_or_create_by_name(name)
+    puts artist
   end
 
   def self.new_by_filename(filename)
